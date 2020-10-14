192f24beeca18e1dc242475225469a9872a3f030
adiletcool <52336435+adiletcool@users.noreply.github.com>
2020-10-15 02:03:25 +0300

    map only with colormapping

--- lib/dataModel.dart
+++ lib/dataModel.dart
@@ -1,15 +1,69 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class CovidTotalDataModel {
-  const CovidTotalDataModel(this.countryName, this.active, {this.covfirmed, this.recovered, this.deaths});
+  const CovidTotalDataModel(this.countryName, this.active, {this.total, this.recovered, this.deaths});
   final String countryName;
   final int active;
-  final int covfirmed;
+  final int total;
   final int recovered;
   final int deaths;
 }
 
-List<CovidTotalDataModel> getTotalData() {
-  return <CovidTotalDataModel>[
-    CovidTotalDataModel('Russia', 500000),
-    CovidTotalDataModel('United States of America', 200000),
-  ];
+Future<http.Response> getStats() async {
+  var headers = {
+    'x-rapidapi-host': "covid-193.p.rapidapi.com",
+    'x-rapidapi-key': "96c9bf6347mshaa70842b22ac472p17ea96jsncd83f482d9aa",
+  };
+  var response = await http.get('https://rapidapi.p.rapidapi.com/statistics', headers: headers);
+  return response;
+}
+
+var replacedName = {
+  'UK': 'United Kingdom',
+  'USA': 'United States of America',
+  'Czechia': 'Czech Rep.',
+  'UAE': 'United Arab Emirates',
+  'Saudi-Arabia': 'Saudi Arabia',
+  'South-Africa': 'South Africa',
+  'CAR': 'Central African Rep.',
+  'DRC': 'Dem. Rep. Congo',
+  'South-Sudan': 'S. Sudan',
+  'Equatorial-Guinea': 'Eq. Guinea',
+  'Burkina-Faso': 'Burkina Faso',
+  'Ivory-Coast': "Côte d'Ivoire",
+  'Western-Sahara': 'W. Sahara',
+  'Sierra-Leone': 'Sierra Leone',
+  'S-Korea': 'Korea',
+  'Laos': 'Lao PDR',
+  'Sri-Lanka': 'Sri Lanka',
+  'Papua-New-Guinea': 'Papua New Guinea',
+  'Solomon-Islands': 'Solomon Is.',
+  'New-Caledonia': 'New Caledonia',
+  'New-Zealand': 'New Zealand',
+  'Bosnia-and-Herzegovina': 'Bosnia and Herz.',
+  'North-Macedonia': 'Macedonia',
+  'Falkland-Islands': 'Falkland Is.',
+  'El-Salvador': 'El Salvador',
+  'Costa-Rica': 'Costa Rica',
+  'Dominican-Republic': 'Dominican Rep.',
+  'Puerto-Rico': 'Puerto Rico'
+};
+
+Future<List<CovidTotalDataModel>> getTotalData() async {
+  var resp = await getStats();
+  List decoded = jsonDecode(resp.body)['response'];
+
+  return List.generate(decoded.length, (i) {
+    var country = decoded[i]['country'];
+    country = replacedName.keys.contains(country) ? replacedName[country] : country;
+    return CovidTotalDataModel(
+      country,
+      decoded[i]['cases']['active'],
+      total: decoded[i]['cases']['total'],
+      recovered: decoded[i]['cases']['recovered'],
+      deaths: decoded[i]['deaths']['total'],
+    );
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:inf_man_project/HexColor.dart';
-import 'package:inf_man_project/constants.dart';
 import 'package:inf_man_project/dataModel.dart';
-import 'package:syncfusion_flutter_charts/charts.dart' hide LabelPlacement;
-import 'package:syncfusion_flutter_sliders/sliders.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
-import 'getData.dart';
-import 'package:dropdown_search/dropdown_search.dart';
-import 'package:intl/intl.dart';
+
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,41 +26,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MapShapeLayerDelegate _mapShapeDelegate;
-  List<CovidTotalDataModel> _covidTotalData;
-  List<MapColorMapper> _colorMappers;
-  MapZoomPanBehavior _zoomPanBehavior;
-
-  Future<void> test(name) async {
-    await getDateByCountry(name);
-  }
-
   @override
   void initState() {
     super.initState();
-    _zoomPanBehavior = MapZoomPanBehavior(maxZoomLevel: 7);
-    _covidTotalData = getTotalData();
-
-    _colorMappers = [
-      MapColorMapper(from: 0, to: 99999, color: Color.fromRGBO(3, 192, 150, 1), text: 'Less than \$1'),
-      MapColorMapper(from: 100000, to: 299999, color: Color.fromRGBO(3, 192, 150, 0.6), text: '\$1 - \$4.99'),
-      MapColorMapper(from: 300000, to: 499999, color: Color.fromRGBO(3, 192, 150, 0.35), text: '\$5 - \$9.99'),
-      MapColorMapper(from: 500000, to: 799999, color: Color.fromRGBO(255, 175, 33, 1.0), text: '\$10 - \$29.99'),
-      MapColorMapper(from: 800000, to: 999999, color: Color.fromRGBO(255, 175, 33, 0.70), text: '\$30 - \$49.99'),
-      MapColorMapper(from: 1000000, to: 10000000, color: Color.fromRGBO(255, 175, 33, 0.40), text: '\$50 and More'),
-    ];
-
-    _mapShapeDelegate = MapShapeLayerDelegate(
-      shapeFile: 'assets/world_map.json',
-      shapeDataField: 'name',
-      dataCount: _covidTotalData.length,
-      primaryValueMapper: (int index) => _covidTotalData[index].countryName,
-      shapeTooltipTextMapper: (int index) {
-        return 'State : ' + (_covidTotalData[index].countryName) + '\nActive : \$' + _covidTotalData[index].active.toString();
-      },
-      shapeColorValueMapper: (int index) => _covidTotalData[index].active,
-      shapeColorMappers: _colorMappers,
-    );
+    getStats();
   }
 
   @override
@@ -74,191 +40,108 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('COVID-19', style: TextStyle(fontSize: 25)),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Flexible(
-            flex: 1,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: Column(
-                    children: [
-                      CountryDropdown(),
-                      SizedBox(height: 20),
-                      MySfSlider(),
-                    ],
-                  ),
+      body: Stack(children: <Widget>[
+        MySfMap(),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              DropdownSearch<String>(
+                mode: Mode.MENU,
+                showSelectedItem: true,
+                items: countriesList,
+                label: "Location",
+                showSearchBox: true,
+                dropdownBuilder: (context, item, isSelected) => Text(item, style: TextStyle(fontSize: 23)),
+                dropdownSearchDecoration: InputDecoration(
+                  labelStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  hoverColor: Colors.red,
+                  contentPadding: EdgeInsets.fromLTRB(12, 12, 0, 0),
+                  border: OutlineInputBorder(),
                 ),
-                Flexible(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: MySfChart(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // Divider(thickness: 1),
-          Flexible(
-            flex: 1,
-            child: MySfMap(_mapShapeDelegate, _zoomPanBehavior),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MySfSlider extends StatefulWidget {
-  @override
-  _MySfSliderState createState() => _MySfSliderState();
-}
-
-class _MySfSliderState extends State<MySfSlider> {
-  List<SliderData> _chartData;
-  final DateTime dateMin = DateTime(2019, 12, 31);
-  final DateTime dateMax = DateTime(2020, 10, 13);
-  SfRangeValues dateValues;
-  List<List<dynamic>> data = [];
-
-  @override
-  void initState() {
-    super.initState();
-    dateValues = SfRangeValues(dateMin, dateMax);
-    _chartData = List.generate(worldCases.length, (i) {
-      return SliderData(x: DateTime.parse(worldCases[i][0]), y: worldCases[i][1]);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 10),
-      child: SfRangeSelector(
-        min: dateMin,
-        max: dateMax,
-        initialValues: dateValues,
-        interval: 1,
-        dateIntervalType: DateIntervalType.years,
-        dateFormat: DateFormat.yMd(),
-        showTooltip: true,
-        child: Container(
-          height: 200,
-          child: SfCartesianChart(
-            margin: const EdgeInsets.all(0),
-            primaryXAxis: DateTimeAxis(
-              minimum: dateMin,
-              maximum: dateMax,
-              isVisible: true,
-            ),
-            primaryYAxis: NumericAxis(isVisible: false, maximum: 400000),
-            series: <SplineAreaSeries<SliderData, DateTime>>[
-              SplineAreaSeries<SliderData, DateTime>(
-                dataSource: _chartData,
-                xValueMapper: (SliderData sales, _) => sales.x,
-                yValueMapper: (SliderData sales, _) => sales.y,
-              )
+                onChanged: (value) => print(value),
+                selectedItem: "Russia",
+                maxHeight: MediaQuery.of(context).size.height / 2,
+              ),
             ],
           ),
         ),
-      ),
+      ]),
     );
   }
 }
 
-class CountryDropdown extends StatefulWidget {
+class MySfMap extends StatefulWidget {
+  const MySfMap();
+
   @override
-  _CountryDropdownState createState() => _CountryDropdownState();
+  _MySfMapState createState() => _MySfMapState();
 }
 
-class _CountryDropdownState extends State<CountryDropdown> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        children: [
-          DropdownSearch<String>(
-            mode: Mode.MENU,
-            showSelectedItem: true,
-            items: countriesList,
-            label: "Location",
-            showSearchBox: true,
-            dropdownBuilder: (context, item, isSelected) => Text(item, style: TextStyle(fontSize: 23)),
-            dropdownSearchDecoration: InputDecoration(
-              labelStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              hoverColor: Colors.red,
-              contentPadding: EdgeInsets.fromLTRB(12, 12, 0, 0),
-              border: OutlineInputBorder(),
-            ),
-            onChanged: (value) => print(value),
-            selectedItem: "Russia",
-            maxHeight: MediaQuery.of(context).size.height / 2,
-          ),
-        ],
-      ),
+class _MySfMapState extends State<MySfMap> {
+  MapShapeLayerDelegate _mapShapeDelegate;
+  List<CovidTotalDataModel> _covidTotalData;
+  List<MapColorMapper> _colorMappers;
+  MapZoomPanBehavior _zoomPanBehavior;
+
+  Future<bool> asyncInit() async {
+    _zoomPanBehavior = MapZoomPanBehavior(maxZoomLevel: 7);
+    _covidTotalData = await getTotalData();
+
+    _colorMappers = [
+      MapColorMapper(from: 0, to: 9999, color: HexColor.fromHex('#ffd460')),
+      MapColorMapper(from: 10000, to: 99999, color: HexColor.fromHex('#f07b3f')),
+      MapColorMapper(from: 100000, to: 499999, color: HexColor.fromHex('#ea5455')),
+      MapColorMapper(from: 500000, to: 999999, color: HexColor.fromHex('#973961')),
+      MapColorMapper(from: 1000000, to: 1499999, color: HexColor.fromHex('#623448')),
+      MapColorMapper(from: 1500000, to: 1999999, color: HexColor.fromHex('#36162e')),
+      MapColorMapper(from: 2000000, to: 3000000, color: HexColor.fromHex('#200f21')),
+      MapColorMapper(from: 5000000, to: 5000002, color: HexColor.fromHex('#200f21')),
+    ];
+
+    _mapShapeDelegate = MapShapeLayerDelegate(
+      shapeFile: 'assets/world_map.json',
+      shapeDataField: 'name',
+      dataCount: _covidTotalData.length,
+      primaryValueMapper: (int index) => _covidTotalData[index].countryName,
+      shapeTooltipTextMapper: (int index) {
+        return 'Country : ' +
+            (_covidTotalData[index].countryName) +
+            '\nActive: ' +
+            _covidTotalData[index].active.toString() +
+            '\nRecovered: ' +
+            _covidTotalData[index].recovered.toString() +
+            '\nDeaths: ' +
+            _covidTotalData[index].deaths.toString() +
+            '\nTotal: ' +
+            _covidTotalData[index].total.toString();
+      },
+      shapeColorValueMapper: (int index) => _covidTotalData[index].active,
+      shapeColorMappers: _colorMappers,
     );
+    return true;
   }
-}
 
-class MySfChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SfCartesianChart(
-      primaryXAxis: CategoryAxis(),
-      series: <LineSeries<SalesData, String>>[
-        LineSeries<SalesData, String>(
-            dataSource: <SalesData>[
-              SalesData('Jan', 35),
-              SalesData('Feb', 28),
-              SalesData('Mar', 34),
-              SalesData('Apr', 32),
-              SalesData('May', 40),
+    return FutureBuilder<bool>(
+      future: asyncInit(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData || snapshot.data == false) return Center(child: CircularProgressIndicator());
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          child: SfMaps(
+            layers: <MapShapeLayer>[
+              MapShapeLayer(
+                // shapeTooltipBuilder: (context, index) => Container(color: Colors.red),
+                delegate: _mapShapeDelegate,
+                enableShapeTooltip: true,
+                zoomPanBehavior: _zoomPanBehavior,
+              )
             ],
-            xValueMapper: (SalesData sales, _) => sales.year,
-            yValueMapper: (SalesData sales, _) => sales.sales,
-            dataLabelSettings: DataLabelSettings(isVisible: true)),
-      ],
+          ),
+        );
+      },
     );
   }
-}
-
-class MySfMap extends StatelessWidget {
-  const MySfMap(this._mapShapeDelegate, this._zoomPanBehavior);
-
-  final MapShapeLayerDelegate _mapShapeDelegate;
-  final MapZoomPanBehavior _zoomPanBehavior;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      child: SfMaps(
-        layers: <MapShapeLayer>[
-          MapShapeLayer(
-            delegate: _mapShapeDelegate,
-            enableShapeTooltip: true,
-            zoomPanBehavior: _zoomPanBehavior,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class SalesData {
-  SalesData(this.year, this.sales);
-  final String year;
-  final double sales;
-}
-
-class SliderData {
-  SliderData({this.x, this.y});
-
-  final DateTime x;
-  final double y;
 }
